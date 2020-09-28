2729d07e9f4c61c6b3387c00e295b94b2b72180b
Krishna <7krishna7dhakal7@gmail.com>
2020-09-28 09:55:44 +0545

    more plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,22 @@ module.exports = {
         path:`${__dirname}/src/components`
       },
     },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve:`gatsby-plugin-mdx`,
+      options:{
+         extensions:[`.md`,`.mdx`],
+         gatsbyRemarkPlugins:[
+           {
+             resolve:`gatsby-remark-images`,
+             options:{
+               maxWidth:1200,
+             },
+           },
+         ],
+      },
+    },
   ],
 }
+ 
